827ad904d0820a4a8e34b5609eacf482006dcdbe
kchekuri26 <57776360+kchekuri26@users.noreply.github.com>
2023-11-04 20:32:07 -0700

    Load done

--- HMM.py
+++ HMM.py
@@ -1,22 +1,35 @@
-
-
 import random
 import argparse
 import codecs
 import os
-import numpy
+import numpy as np
+
 
 # observations
 class Observation:
     def __init__(self, stateseq, outputseq):
-        self.stateseq  = stateseq   # sequence of states
+        self.stateseq = stateseq  # sequence of states
         self.outputseq = outputseq  # sequence of outputs
+
     def __str__(self):
-        return ' '.join(self.stateseq)+'\n'+' '.join(self.outputseq)+'\n'
+        return ' '.join(self.stateseq) + '\n ' + ' '.join(self.outputseq) + '\n'
+
     def __repr__(self):
         return self.__str__()
+
     def __len__(self):
         return len(self.outputseq)
+
+
+def load_helper(filename, dictionary):
+    with codecs.open(filename, 'r', 'utf-8') as file:
+        for line in file:
+            parts = line.strip().split()
+            if len(parts) == 3:
+                if parts[0] not in dictionary:
+                    dictionary[parts[0]] = {}
+                dictionary[parts[0]][parts[1]] = float(parts[2])
+
 
 # hmm model
 class HMM:
@@ -32,17 +45,12 @@ class HMM:
 
     ## part 1 - you do this.
     def load(self, basename):
-        """reads HMM structure from transition (basename.trans),
-        and emission (basename.emit) files,
-        as well as the probabilities."""
+        load_helper(f"{basename}.trans", self.transitions)
+        load_helper(f"{basename}.emit", self.emissions)
 
-
-
-   ## you do this.
+    ## you do this.
     def generate(self, n):
         """return an n-length observation by randomly sampling from this HMM."""
-
-
 
     ## you do this: Implement the Viterbi alborithm. Given an Observation (a list of outputs or emissions)
     ## determine the most likely sequence of states.
@@ -52,8 +60,3 @@ class HMM:
         find and return the state sequence that generated
         the output sequence, using the Viterbi algorithm.
         """
-
-
-
-
-
